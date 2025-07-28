f00ebd64546d23219d6f0fc20eadaa87cd794c2c
CrepusShop <crepusinconnu@gmail.com>
2025-07-28 17:59:36 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
 import json
-import os
 
 app = Flask(__name__)
 
@@ -27,5 +26,4 @@ def check_license():
         return jsonify({"status": "expired", "message": "Licence expirée"}), 403
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
